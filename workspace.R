5851c18188a9f451339b688340dc520eaf6b7e48
Faris Naji <fnaji@pamgene.com>
2020-08-09 15:56:35 +0200

    added sample info

--- workspace.R
+++ workspace.R
@@ -16,9 +16,8 @@ measurements<- cbind(row_id, gene_id, measurements)
 write.table(measurements, file = file.path("data", "yoruba_data.tsv"), sep="\t", quote=FALSE, row.names = FALSE)
 
 gene_annot<-y$genes
-gene_id <- rownames(gene_annot)
 row_id <- 1:nrow(gene_annot)
-gene_annot <- cbind(row_id, gene_id, gene_annot)
+gene_annot <- cbind(row_id, gene_annot)
 write.table(gene_annot, file = file.path("data", "yoruba_gene_annot.tsv"), sep="\t", quote=FALSE, row.names = FALSE)
 
 sample_annot<-y$samples
